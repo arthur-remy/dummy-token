c94ad803b3dc3aad8319811bf1ffb6aa106cef89
Arthur <arthur@coeos.xyz>
2023-04-25 18:34:01 +0200

    Add decimal support

--- contracts/multiple-admin-dummy-token.sol
+++ contracts/multiple-admin-dummy-token.sol
@@ -5,25 +5,35 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract DummyToken is ERC20, AccessControl {
-    uint _tokenValue;
+    uint public tokenValue;
+    uint8 public decimals;
     bytes32 public constant ADMINS = keccak256("ADMINS");
 
     constructor(
         string memory tokenName,
         string memory tokenSymbol,
-        uint tokenValue
+        uint8 _decimals,
+        uint _tokenValue
     ) ERC20(tokenName, tokenSymbol) {
-        _tokenValue = tokenValue;
+        tokenValue = _tokenValue;
+        decimals = _decimals;
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
         _setupRole(ADMINS, _msgSender());
     }
-
+    
     function mint(address to) external payable {
-        _mint(to, msg.value * _tokenValue);
+        int decimalDiff = int8(decimals()) - 18;
+        if (decimalDiff >= 0) {
+            _mint(to, msg.value * tokenValue * 10 ** uint(decimalDiff));
+        } else {
+            require(msg.value >= 10 ** uint(decimalDiff), "DummyToken: Not enough funds");
+            _mint(to, msg.value * tokenValue / 10 ** uint(-decimalDiff));
+        }
     }
     
     function burn(uint256 amount) external {
-        require(amount >= _tokenValue);
+        uint additionalDecimals = decimals() - 18;
+        require(amount >= _tokenValue * 10 ** additionalDecimals);
         _burn(_msgSender(), amount);
         (bool sent, bytes memory data) = _msgSender().call{value: amount / _tokenValue}("");
         require(sent, "Failed to send Ether");
