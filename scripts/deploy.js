291e71195f8c7fdfaf5be8965fafb4e302cdbc5d
Arthur <arthur@coeos.xyz>
2023-04-25 22:36:59 +0200

    Add token decimals to deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,11 +2,11 @@ const hre = require("hardhat");
 
 async function main() {
   const DummyToken = await hre.ethers.getContractFactory("TestnetToken");
-  const alpha = await TestnetToken.deploy('AlphaToken', 'ALPHA', 2);
+  const alpha = await TestnetToken.deploy('AlphaToken', 'ALPHA', 18, 2);
   await alpha.deployed();
-  const beta = await TestnetToken.deploy('BetaToken', 'BETA', 10);
+  const beta = await TestnetToken.deploy('BetaToken', 'BETA', 18, 10);
   await beta.deployed();
-  const gamma = await TestnetToken.deploy('GammaToken', 'GAMMA', 30);
+  const gamma = await TestnetToken.deploy('GammaToken', 'GAMMA', 18, 30);
   await gamma.deployed();
   console.log(alpha);
 }
